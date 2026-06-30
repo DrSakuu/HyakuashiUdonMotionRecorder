7b003a0ea6dfd032d046a2c4b5a1afa23bd22503
DrSakuu <sakuukuli@gmail.com>
2026-06-30 20:07:48 +0300

    Add RecordLogLoader.cs and its CustomEditor

---
+++ Editor/RecordLogLoaderEditor.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace HUMR
+{
+    [CustomEditor(typeof(RecordLogLoader))]
+    public class RecordLogLoaderEditor : Editor
+    {
+        private bool _showAdvanced;
+        
+        public override void OnInspectorGUI()
+        {
+            
+            var recordLoader = (RecordLogLoader)target;
+            if (recordLoader == null) return;
+
+            _showAdvanced = EditorGUILayout.Foldout(_showAdvanced, "Advanced: Custom Log Path");
+            if (_showAdvanced)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.BeginHorizontal();
+                recordLoader.path = EditorGUILayout.TextField("OutputLogPath", recordLoader.path);
+                if (GUILayout.Button("Explore", GUILayout.Width(100)))
+                {
+                    var path = Environment.GetEnvironmentVariable("USERPROFILE") + @"\AppData\LocalLow\VRChat\VRChat";
+                    if (Directory.Exists(path))
+                    {
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = path,
+                            UseShellExecute = true,
+                            Verb = "open"
+                        });
+                    }
+                    else
+                    {
+                        UnityEngine.Debug.LogError($"Log path does not exist: {path}");
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+                EditorGUI.indentLevel--;
+            }
+            else
+            {
+                recordLoader.path = Environment.GetEnvironmentVariable("USERPROFILE");
+                recordLoader.path += @"\AppData\LocalLow\VRChat\VRChat";
+            }
+            
+            if (recordLoader.logFileNames == null) recordLoader.CollectLogFiles();
+
+            const string label = "Record Log";
+            var controlRect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight);
+            var popupRect = EditorGUI.PrefixLabel(controlRect, new GUIContent(label));
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 0 && popupRect.Contains(Event.current.mousePosition))
+            {
+                recordLoader.CollectLogFiles();
+            }
+
+            if (recordLoader.logFileNames != null && recordLoader.logFileNames.Length > 0)
+            {
+                recordLoader.logFileIndex = EditorGUI.Popup(popupRect, recordLoader.logFileIndex, recordLoader.logFileNames);
+            }
+            else
+            {
+                var emptyOptions = new string[] { "Log files not Found" };
+                EditorGUI.Popup(popupRect, 0, emptyOptions);
+            }
+        }
+    }
+}
