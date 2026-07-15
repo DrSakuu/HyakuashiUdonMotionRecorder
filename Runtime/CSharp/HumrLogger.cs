5301213627c38ffd28c6abfeb9d8c76c079b1a7b
DrSakuu <sakuukuli@gmail.com>
2026-07-15 12:16:24 +0300

    HumrLogger: Adjust formatting precision

--- Runtime/CSharp/HumrLogger.cs
+++ Runtime/CSharp/HumrLogger.cs
@@ -9,6 +9,7 @@ namespace HUMR
         public const string RecordingTag = "RECORDING";
         public const char VariableDelimiter = ';';
         public const char ComponentDelimiter = ',';
+        public const string FloatFormat = "F6";
 
         public static void Log(object message)
         {
@@ -30,16 +31,21 @@ namespace HUMR
             Debug.LogAssertion($"{HumrTag} {message}");
         }
 
-        public static string FormatVector3Components(Vector3 vector3)
+        public static string FormatVector3Components(Vector3 vector3, string format=FloatFormat)
         {
-            var trimmedVector3 = vector3.ToString().Trim('(', ')');
-            return trimmedVector3.Replace(" ", "");
+            var vector3XStr = vector3.x.ToString(format);
+            var vector3YStr = vector3.y.ToString(format);
+            var vector3ZStr = vector3.z.ToString(format);
+            return string.Join(ComponentDelimiter, vector3XStr, vector3YStr, vector3ZStr);
         }
 
-        public static string FormatQuaternionComponents(Quaternion quaternion)
+        public static string FormatQuaternionComponents(Quaternion quaternion, string format=FloatFormat)
         {
-            var trimmedQuaternion = quaternion.ToString().Trim('(', ')');
-            return trimmedQuaternion.Replace(" ", "");
+            var quaternionXStr = quaternion.x.ToString(format);
+            var quaternionYStr = quaternion.y.ToString(format);
+            var quaternionZStr = quaternion.z.ToString(format);
+            var quaternionWStr = quaternion.w.ToString(format);
+            return string.Join(ComponentDelimiter, quaternionXStr, quaternionYStr, quaternionZStr,  quaternionWStr);
         }
 
         public static string RecordingTypeToString(FrameType frameType)
